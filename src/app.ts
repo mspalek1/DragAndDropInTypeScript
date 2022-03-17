c9f86b4c61ffdd384ced14a8e1e0e8b88f2f45dc
Maciej Spaleniak <maciej.spaleniak@comarch.pl>
2022-03-17 10:20:38 +0100

    rendering projcet items use a class

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ enum ProjectStatus {
 class Project {
     constructor(public id: string, 
         public title: string, 
-        public descirption: string,
+        public description: string,
         public people: number,
         public status: ProjectStatus){}
 }
@@ -130,6 +130,27 @@ abstract class  Component<T extends HTMLElement, U extends HTMLElement> {
     abstract renderContent(): void;
 }
 
+
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+    private project: Project;
+
+    constructor(hostId: string, project: Project) {
+        super('single-project', hostId, false, project.id);
+        this.project = project;
+        this.configure();
+        this.renderContent();
+    }
+
+    configure(): void {
+        
+    }
+    renderContent(): void {
+        this.element.querySelector('h2')!.textContent = this.project.title;
+        this.element.querySelector('h3')!.textContent = this.project.people.toString();
+        this.element.querySelector('p')!.textContent = this.project.description;
+    }
+}
+
 //ProjectList Class
 class ProjectList extends Component<HTMLDivElement, HTMLElement>{
     assignedProject: Project[];
@@ -148,9 +169,14 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement>{
         listEl.innerHTML = '';
 
         for (const prjItem of this.assignedProject) {
-            const listItem = document.createElement('li');
-            listItem.textContent = prjItem.title;
-            listEl.appendChild(listItem);
+            //create manualy
+
+            // const listItem = document.createElement('li');
+            // listItem.textContent = prjItem.title;
+            // listEl.appendChild(listItem);
+
+            //create as object class
+            new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
         }
     }
 
